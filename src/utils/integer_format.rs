06e2dfb8d401efc7f119ef3777b3d9a4c9cf0531
Jose Leiva <email.joseleiva@gmail.com>
2026-07-17 20:27:43 -0400

    fix: fix superscripts wrong array order

--- src/utils/integer_format.rs
+++ src/utils/integer_format.rs
@@ -1,4 +1,4 @@
-const SUPERSCRIPTS: [char; 10] = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁹', '⁶', '⁸', '⁷'];
+const SUPERSCRIPTS: [char; 10] = ['⁰', '¹', '²', '³', '⁴', '⁵', '⁶', '⁷', '⁸', '⁹'];
 
 pub fn usize_to_superscript(num: usize) -> String {
     num.to_string()
